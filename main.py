c628f8b75ada454f616163e95fba25938a3a8c90
Ben W <Bwesterhof14@gmail.com>
2022-03-25 20:34:27 -0400

    New branch and wild Magic table

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ import os
 import discord
 from dotenv import load_dotenv
 
+class potatoBot():
+    def __init__(self):
+        self.wildMagic = self.readWildMagic()
+
+    #Reads the d10,000 list of wild magic options from the text file and saves it in a list
+    #Inputs: N/A
+    #Outputs: list
+    def readWildMagic(self):
+        wildMagic = []
+        file = ".\\d10,000 table.txt"
+        with open(file, "r+") as localFile:
+            for line in localFile:
+                wildMagic.append(line)
+        localFile.close()
+        return wildMagic
+
+    
+    #Simulates a roll on the wild magic table and returns the result
+    #Inputs: N/A
+    #Outputs: String
+    def rollWildMagic(self):
+        #Roll a random number, 0 to 9999, and use that to index the list
+        pass
+
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
